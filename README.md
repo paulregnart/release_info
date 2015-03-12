7b4f128edc4c7627bc2b4b5b170d1c6441c3ee30
Paul Regnart <paul.regnart@gmail.com>
2015-03-12 20:27:25 +0000

    getting tags

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This fetchs tags and groups them, by Release Candidate Version
+This fetchess tags and groups them, by Release Candidate Version
 Run in the root directory of your local git repo
 
 The aim is to see how often a version is released with first time quality (RC1)
---
+++ ReleaseInfo.rb
@@ -0,0 +1,15 @@
+class ReleaseInfo
+  `git fetch --tags`
+  tags = `git tag`.split("\n")
+
+  puts "There are #{tags.count} tags for this repo."
+  @rc_hash = Hash.new{|h,k| h[k] = 0 }
+
+  tags.each do |tag|
+    if tag.include?("rc")
+      tag_rc = tag.split(".").last
+      @rc_hash[tag_rc.to_sym] += 1
+    end
+  end
+  puts @rc_hash
+end
