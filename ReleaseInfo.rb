566689248a8c3bea94a98764ca50596c2350a956
Paul Regnart <paul.regnart@gmail.com>
2015-03-13 05:30:26 +0000

    formatting

--- ReleaseInfo.rb
+++ ReleaseInfo.rb
@@ -3,18 +3,18 @@ class ReleaseInfo
   tags = `git tag`.split("\n")
 
   puts "There are #{tags.count} tags for this repo."
-  @rc_hash = Hash.new{|h,k| h[k] = 0 }
+  @rc_hash = Hash.new { |h, k| h[k] = 0 }
 
   tags.each do |tag|
     if tag.include?(".rc")
       tag_rc = tag.split(".").last
-      @rc_hash[tag_rc.gsub('rc','').to_i] += 1
+      @rc_hash[tag_rc.gsub('rc', '').to_i] += 1
     end
   end
 
   result = Hash[@rc_hash.sort]
 
-	result.each do |key, value| 
-		puts "RC #{key} " + "*" * value.to_i
-	end
+  result.each do |key, value|
+    puts "RC #{key} " + "*" * value.to_i
+  end
 end
